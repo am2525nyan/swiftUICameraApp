51015f5c6239d1d781ddf88793c18fc75637330c
saki0411 <chipdale411@gmail.com>
2024-11-03 13:16:13 +0900

    Initial Commit

---
+++ CameraContorol/CameraContorolApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraContorolApp.swift
+//  CameraContorol
+//
+//  Created by saki on 2024/11/03.
+//
+
+import SwiftUI
+
+@main
+struct CameraContorolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
